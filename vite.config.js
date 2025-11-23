915ecb8b914cdcc80e3d2e24197f3e623c59f54c
rifqi <afatkhurrifqi11@gmail.com>
2025-11-23 13:10:54 +0700

    Dukungan offline dengan workbox

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,33 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+      },
+      includeAssets: ['favicon.ico'],
+      manifest: {
+        name: 'Luxspace Ecommerce',
+        short_name: 'Luxspace',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: '/images/favicon.ico',
+            sizes: '48x48',
+            type: 'image/x-icon',
+          },
+        ],
+      },
+    }),
+    tailwindcss(),
+  ],
 });
